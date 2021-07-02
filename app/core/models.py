0fdfd910e9af3ee21830fa62f22b165f331ad290
Gaspar Neto <gcsnetoo@gmail.com>
2021-07-02 10:08:59 -0300

    Updated admin to support custom user model.

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
+from django.utils import timezone
 
 
 class UserManager(BaseUserManager):
@@ -35,6 +36,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     objects = UserManager()
 
